88f57a9240d0cf9a443c8481eb1545b19b4ae619
Tejeshwar Reddy <mbtejeshwarreddy@gmail.com>
2021-02-14 23:25:46 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
+sudo apt install -y npm
 node -e "console.log('Running Node.js ' + process.version)"
+
 
 
 # Steps to install MongoDB
